9a2a8f4aeed6bc33f2ad663b23f2179def78f54a
tiago simao <tiagosimaorodri123@gmail.com>
2025-11-24 01:50:18 -0300

    criado o pedido listener no service de notificação.

--- notificacao-service/src/main/java/br/com/pedidos/notificacao/listener/PedidoListener.java
+++ notificacao-service/src/main/java/br/com/pedidos/notificacao/listener/PedidoListener.java
@@ -6,14 +6,25 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import br.com.pedidos.notificacao.entity.Pedido;
+import br.com.pedidos.notificacao.service.EmailService;
 
 @Component
 public class PedidoListener {
 
+	
+	
 	private final Logger logger = LoggerFactory.getLogger(PedidoListener.class);
+	
+	private final EmailService emailService;
+	
+	public PedidoListener(EmailService emailService ) {
+		this.emailService = emailService;
+	}
+	
 	
 	@RabbitListener(queues = "pedidos.v1.pedido-criado.gerar-notificacao" )
 	public void enviarNotificacao(Pedido pedido) {
+		emailService.enviarEmail(pedido);
 		logger.info("Notificação gerada: {}", pedido.toString());
 	}
 	
